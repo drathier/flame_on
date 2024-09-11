b82fd6bddb82817b75eb76a678d35eaaf2beae09
drathier <github@fi.fo>
2024-09-11 12:42:57 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FlameOn.MixProject do
   def project do
     [
       app: :flame_on,
-      version: "0.5.2",
+      version: "0.5.3",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
